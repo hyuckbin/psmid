94cb02266236f3d09d068dc8bcb40a99a3f73acb
hyuckbin <rpdak159@naver.com>
2025-04-24 19:29:52 +0900

    Wrapper

---
+++ src/main/java/org/example/yalco/Wrap.java
@@ -0,0 +1,39 @@
+//package org.example.yalco;
+//
+//public class Wrap {
+//    //  💡 숫자 클래스 메소드들
+//
+//    //  CharSequence로부터 인스턴스 반환
+//    //  ⭐ CharSequence : String 등이 구현하는 인터페이스
+//    Integer int1 = Integer.valueOf("123"); // 문자열로부터 인스턴스 반환
+//
+//    //  CharSequence로부터 원시값 반환
+//    //  💡 다른 숫자, 불리언 래퍼 자료형들에도 존재 (parseDouble, parseBoolean...)
+//    int int2 = Integer.parseInt("123"); // 원시값 반환
+//
+//    //  parseInt(CharSequence, 진수)
+//    //  정수 자료형들에만 존재
+//    //  ⭐ CharSequence : String 등이 구현하는 인터페이스
+//    int int_123_oct = Integer.parseInt("123", 8);
+//    int int_123_dec = Integer.parseInt("123", 10);
+//    int int_123_hex = Integer.parseInt("123", 16);
+//
+//    //  parseInt(CharSequence, 시작위치, 끝위치, 진수)
+//    int int3 = Integer.parseInt("1234567", 3, 5, 10);
+//
+//    //  💡 문자 클래스 메소드들
+//
+//    String strSample = "Ab가1 .";
+//        for (int i = 0; i < strSample.length(); i++) {
+//        Character c = strSample.charAt(i);
+//        System.out.printf(
+//                "[%c] : L: %b, U: %b, L: %b, D: %b, S: %b%n",
+//                c,
+//                Character.isLetter(c),
+//                Character.isUpperCase(c),
+//                Character.isLowerCase(c),
+//                Character.isDigit(c),
+//                Character.isSpaceChar(c)
+//        );
+//    }g
+//}
--- src/main/java/org/example/yalco/enumer.java
+++ src/main/java/org/example/yalco/enumer.java
@@ -2,7 +2,9 @@ package org.example.yalco;
 
 public class enumer {
     //고정된 상수값들을 사용할 때 사용
-    public enum ButtonMode{
-        Light,Dark
-    }
+//    public enum ButtonMode{
+//        Light,Dark
+//    }
+
+    enum Mode { Dark,Light }
 }
